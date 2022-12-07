238adb89e8f108aec7602af78c1981ad16974c27
MaximeFouche <114243800+MaximeFouche@users.noreply.github.com>
2022-12-07 15:17:44 +0100

    Update Projmax.py

--- Projmax.py
+++ Projmax.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import numpy as np
@@ -5,7 +6,7 @@ import matplotlib.pyplot as plt
 
 #Question 5
 
-def LU2(A):
+def LU2(A):         #Renvoie la décomposition LU de A
     n = len(A)
     L = np.eye(n)
     U = np.zeros((n, n))
@@ -17,7 +18,7 @@ def LU2(A):
             L[k][j] = 1/U[j][j] * (A[k][j] -L[k,:j] @ U[:j,j])
     return L,U
 
-def remontee(A,b):
+def remontee(A,b):          #Résout Ax=b avec A triangulaire supérieure
     x = np.copy(b)
     n = len(b)
     for i in range(n-1, -1, -1):
@@ -25,7 +26,7 @@ def remontee(A,b):
         x[i] = x[i] / A[i][i]
     return x
 
-def descente(A,b):
+def descente(A,b):       #Résout Ax=b avec A triangulaire inférieure
     x = np.copy(b)
     n = len(b)
     for i in range(n):
@@ -33,7 +34,7 @@ def descente(A,b):
         x[i] = x[i] / A[i][i]
     return x
 
-def MatriceA(n,tau):
+def MatriceA(n,tau):            #Crée la matrice A
     t = np.arange(n)
     V = 1 / (1 + t**2 * tau**2)
     
@@ -44,14 +45,14 @@ def MatriceA(n,tau):
         A += d + d.T
     return A
 
-def MatP(n,panne):
+def MatP(n,I):                     #Crée P selon la taille n et l'ensemble I des antennes en panne
         P=np.eye(n)
         for i in range(n):
-            if i in panne:
+            if i in I:
                 P[i,i]=0
         return P
     
-def MatAnz(A):
+def MatAnz(A):                       #Retourne une matrice sans les colonnes et lignes nulles de A
     C=[]
     for i in range(len(A)):
         X=[]
@@ -63,7 +64,7 @@ def MatAnz(A):
     C=np.array(C)
     return C
 
-def Vectnz(V):
+def Vectnz(V):                  #Renvoie un vecteur V privé de ses coefficients nuls
     C=[]
     for i in range(len(V)):
         if V[i]!=0:
@@ -72,61 +73,62 @@ def Vectnz(V):
     return C
 
 
-def xfinal(x,panne):
+def xfinal(x,I):             #Ajoute les 0 aux indices correspondants pour donner le xf solution de (8) ou (10)
     c=0
-    n=len(x)+len(panne)
+    n=len(x)+len(I)
     xf=np.zeros(n)
     for i in range(n):
-        if i not in panne:
+        if i not in I:
             xf[i]=x[c]
             c+=1
     return xf
 
 A=MatriceA(20,1)
-panne=(6,7,14,15,16,17)
-P=MatP(20,panne)
+pi=np.ones(20)
+I=(6,7,14,15,16,17)  #Ensemble des pannes avec un décalage de -1, les matrices commencent en 0 sous Python
+P=MatP(20,I)
 
 PAP=P@A@P
-PAPr=MatAnz(PAP)
-pir=np.ones(14)
-pif=np.ones(20)
+PAPr=MatAnz(PAP)   #Version réduite de PAP
+Ppi=P@pi
+Ppir=Vectnz(Ppi) #Version réduite de Ppi
 L,U=LU2(PAPr)
 
-y=descente(L,pir)
+y=descente(L,Ppir)
 x=remontee(U,y)
-xf=xfinal(x,panne)
+xf=xfinal(x,I)
 
 print(xf) 
-print(np.allclose(PAP@xf, P@pif)) 
-print(np.allclose(P@xf, xf)) 
+print(np.allclose(PAP@xf, Ppi))       #Vérifie que PAPx=Ppi
+print(np.allclose(P@xf, xf))            #Vérifie que Px=x
 
 Ax = A@xf
 plt.plot(Ax, label="Ax=π")
-plt.step(np.arange(20),xf, label="x")
+plt.step(np.arange(20),xf, label="x")   #Affichage en escalier
 plt.title('Question 5')
 plt.legend()
-plt.show()
+plt.show()               #Permet d'afficher les deux graphes sur deux plots différents (on sépare Question 5 et 6)
 
 #Question 6
 A=MatriceA(20,1)
-panne=(6,7,14,15,16,17)
-P=MatP(20,panne)
-pif=np.ones(20)
+I=(6,7,14,15,16,17)  #Ensemble des pannes décalé de -1
+P=MatP(20,I)
+pi=np.ones(20)
 
 PAAP=P@A.T@A@P
-PApi=P@A.T@pif
-PAAPr=MatAnz(PAAP)
-PApir=Vectnz(PApi)
+PApi=P@A.T@pi
+PAAPr=MatAnz(PAAP)   #Version réduite de PAAP
+PApir=Vectnz(PApi)   #Version réduite de PApi
 
 L1,U1=LU2(PAAPr)
 
 y1=descente(L1,PApir)
 x1=remontee(U1,y1)
-xf1=xfinal(x1,panne)
+xf1=xfinal(x1,I)
 
 print(xf1) 
-print(np.allclose(PAAP@xf1, PApi)) 
-print(np.allclose(P@xf, xf)) 
+print(np.allclose(PAAP@xf1, PApi)) #Vérifie que PA.TAPx=PA.Tpi
+print(np.allclose(P@xf, xf))       #Vérifie que Px=x
 
 
 plt.plot(A@xf1, label="Ax=π")
